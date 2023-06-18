f3778eafb26a655bf9fbb05094ecb4ad0d5a8daf
Bhavesh chaudhari <bhaveshchaudhari909@gcoea.ac.in>
2023-06-18 20:35:31 +0530

    Completed the Education and Experiment part

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const data = [
+  {
+    id: 1,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2019 - present",
+    title: "B.Tech in Information Technology",
+    desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+  },
+  {
+    id: 2,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2018 - 2021",
+    title: "Diploma in Electrical Engineering",
+    desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+  },
+  {
+    id: 3,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2019 - present",
+    title: "Web Designer",
+    desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+  },
+];
+
+export default data
